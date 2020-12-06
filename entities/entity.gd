fba59e6aa8a1793fdf0e99893afc42b4a9f7691e
Robin Andersson <roban591@student.liu.se>
2020-12-06 14:04:37 +0100

    Implement astar pathfinding and add an alternative that uses it.

--- entities/entity.gd
+++ entities/entity.gd
@@ -10,8 +10,6 @@ export var trauma: float
 export var fire_rate: float
 export var bullet_scene: PackedScene
 
-var level_size: Rect2 setget set_level_size
-
 var _velocity: Vector2 = Vector2.ZERO
 var _acceleration: Vector2 = Vector2.ZERO
 
@@ -27,15 +25,6 @@ onready var _explosion: Particles2D = $Explosion
 
 func _ready() -> void:
 	_bullet_delay.wait_time = fire_rate
-
-
-func set_level_size(new_level_size: Rect2) -> void:
-	level_size = new_level_size
-
-
-func _handle_out_of_bounds() -> void:
-	position.x = clamp(position.x, level_size.position.x, level_size.end.x)
-	position.y = clamp(position.y, level_size.position.y, level_size.end.y)
 
 
 func _on_BulletDelay_timeout() -> void:
