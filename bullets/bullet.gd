752bc21e1915b461affd4f7edd875bbadd0189de
Robin Andersson <roban591@student.liu.se>
2020-12-11 15:55:16 +0100

    Change bullet sprites to better fit with the art of the rest of the game.

--- bullets/bullet.gd
+++ bullets/bullet.gd
@@ -29,10 +29,6 @@ func _explode() -> void:
 	set_physics_process(false)
 
 
-func _on_VisibilityNotifier2D_screen_exited() -> void:
-	queue_free()
-
-
 func _on_Bullet_body_entered(body: PhysicsBody2D) -> void:
 	if body is Entity:
 		body.hurt(_damage)
